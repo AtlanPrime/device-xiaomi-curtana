62592271b2dc6ed6155e6ff660588446048203b3
Jyotiraditya <imjyotiraditya@pm.me>
2020-10-07 10:17:28 +0000

    curtana: Build signapk jar
    Test: build
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Change-Id: Iab633fc9d450ec1354308a87f300550df73553bb

--- device.mk
+++ device.mk
@@ -202,6 +202,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
